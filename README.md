f6c2da3af463709cbcc046549d4d6a0510b11595
yorkie <yorkiefixer@gmail.com>
2016-01-29 20:37:15 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,138 @@
+# Graphml-views-mongodb
+
+[![NPM version][npm-image]][npm-url]
+[![Build status][travis-image]][travis-url]
+[![Dependency Status][david-image]][david-url]
+[![Downloads][downloads-image]][downloads-url]
+
+A library that enable views for MongoDB based on [GraphML], which generates collections 
+from MongoDB datasources by given graphml files.
+
+## Installation
+
+```sh
+$ npm install graphml-views-mongodb --save
+```
+
+## Why create this?
+
+The original MongoDB doesn't support view like CouchDB and other SQL-Like database. And
+it supports the following ways to make relations between multiple collections:
+
+- embed documents
+- `DBRef` type to reference to other database
+
+### weakness of the above ways to relate collections
+
+The embed documents even though has a avilability for retrieving data, but lack of consistency
+on writing data on the embeded documents.
+
+The `DBRef` is not recommended way to take the role of expressing relations, we should use it
+to reference a value to another database that maybe in other system.
+
+### about loopback.io
+
+The next context is about [Loopback], the framework to replace the role of Parse and LeanCloud,
+the framework abstracts most popluar databases into the same one schema, and it support rich relations
+as below:
+
+- one-to-one
+- one-to-many
+
+That's good, but not enough, because [Loopback]'s scoping/joining syntax is such complicated so that
+the request URLs would become too long and unreadability. So sometimes, the truth is that we want a collection
+is for a view, and we just query it directly from the collection without scoping and joining. At the same time,
+for write operations, we hope that model-based would be used and it will updates data to the view collections.
+
+### why GraphML?
+
+In starting this project, we tried JSON, YAML and TOML to express what fields would be in our view collection,
+but they are almost out after having a try on GraphQL, and then we found GraphQL is the QL for querying, so we,
+the [WeFlex] team decoded to rewrite a new marked language based on basic of [GraphQL], that's the [GraphML].
+
+To express a view, you just need the following:
+
+```
+User {
+  username,
+  nickname,
+  sex,
+  country,
+  orgs {
+    name
+  },
+  followings {
+    username,
+    avatarUrl
+  },
+  followers {
+    username,
+    avatarUrl
+  }
+}
+```
+
+Then you will get document:
+
+```JSON
+{
+  "_id": "id",
+  "username": "yorkie",
+  "nickname": "yorkie",
+  "sex": "female",
+  "country": "internet",
+  "orgs": [
+    {
+      "_id": "weflex_id",
+      "name": "weflex"
+    }
+  ],
+  "followings": [
+    {
+      "_id": "following_id",
+      "name": "substack",
+      "avatarUrl": "image url"
+    }
+  ],
+  "followers": [
+    {
+      "_id": "follower_id",
+      "name": "scott",
+      "avatarUrl": "image url"
+    }
+  ]
+}
+```
+
+### How it works
+
+The [graphml-views-mongodb] does parse a GraphML file and get fields collection that you want to include from
+the source collections, and read the relations from pre-defined [Loopback] models, and generate the complete
+documents.
+
+## API
+
+- `parseGraphs(models, dataSources)`
+  - `models` {Array} the object returned from loopback
+  - `dataSources` {Object} the object to take dataSources
+    - `rest` {MongoDB} the resouce database instance
+    - `view` {MongoDB} the view database instance
+
+## License
+
+MIT @ WeFlex, Inc.
+
+[GraphQL]: https://github.com/facebook/graphql
+[GraphML]: https://github.com/weflex/graphml
+[Loopback]: https://github.com/strongloop/loopback
+[WeFlex]: https://github.com/weflex
+
+[npm-image]: https://img.shields.io/npm/v/graphml-views-mongodb.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/graphml-views-mongodb
+[travis-image]: https://img.shields.io/travis/weflex/graphml-views-mongodb.svg?style=flat-square
+[travis-url]: https://travis-ci.org/weflex/graphml-views-mongodb
+[david-image]: http://img.shields.io/david/weflex/graphml-views-mongodb.svg?style=flat-square
+[david-url]: https://david-dm.org/weflex/graphml-views-mongodb
+[downloads-image]: http://img.shields.io/npm/dm/graphml-views-mongodb.svg?style=flat-square
+[downloads-url]: https://npmjs.org/package/graphml-views-mongodb
+
---
+++ index.js
@@ -0,0 +1,263 @@
+"use strict";
+
+const _ = require('lodash');
+const fs = require('fs');
+const moment = require('moment');
+const graphml = require('graphml-parser');
+const glob = require('glob').sync;
+const path = require('path');
+const ObjectID = require('mongodb').ObjectID;
+
+class MongoView {
+  constructor(name, source, options) {
+    const ast = graphml.parse(source);
+    this.models = options.models;
+    this.dataSources = options.dataSources;
+    this.name = name;
+    this.rootType = ast.type;
+    this.keys = [
+      {
+        type: this.rootType,
+        idKey: (id) => {
+          return {
+            _id: ObjectID(id)
+          };
+        }
+      }
+    ];
+    this.rootTree = this.parseTreeWithModels(ast.root);
+  }
+  parseTreeWithModels(ast) {
+    let type = ast.type;
+    if (!type) {
+      type = ast.type = this.rootType;
+      ast.isRoot = true;
+    }
+    let model = this.models[type];
+    let relations = model.relations;
+    for (let name in ast.methods) {
+      let method = ast.methods[name];
+      let relation = relations[name];
+      if (method.args && method.args.$relation) {
+        relation = relations[method.args.$relation];
+      }
+      if (!relation) {
+        throw new TypeError('cannot handle with method: ' + name);
+      }
+      method.name = name;
+      method.type = relation.model;
+      method.relation = relation;
+      if (ast.isRoot && method.relation.type === 'hasMany') {
+        method.filterConfig = {
+          name: method.name,
+          base: {
+            [method.name]: {
+              $elemMatch: null
+            }
+          },
+          hasArray: true,
+          isHasMany: true
+        };
+      } else {
+        let parentConfig = method.parent.filterConfig;
+        let currentConfig = {
+          name: method.name
+        };
+        if (!parentConfig) {
+          currentConfig.base = currentConfig.name;
+        } else {
+          if (parentConfig.hasArray === true) {
+            currentConfig.hasArray = true;
+            if (parentConfig.isHasMany) {
+              currentConfig.superBase = parentConfig;
+              currentConfig.base = currentConfig.name;
+            } else {
+              currentConfig.superBase = parentConfig.superBase;
+              currentConfig.base = parentConfig.base + '.' + currentConfig.name;
+            }
+          } else {
+            currentConfig.base = parentConfig.base + '.' + currentConfig.name;
+          }
+        }
+        method.filterConfig = currentConfig;
+      }
+      this.keys.push({
+        type: relation.model,
+        idKey: (id) => {
+          let config = method.filterConfig;
+          id = ObjectID(id);
+          if (!config) {
+            return {_id: id};
+          } else if (config.hasArray === true) {
+            let filter;
+            if (config.superBase) {
+              filter = Object.assign({}, config.superBase.base);
+              filter[config.superBase.name].$elemMatch = {
+                [config.base + '._id']: id
+              };
+            } else {
+              filter = Object.assign({}, config.base);
+              filter[config.name].$elemMatch = {
+                _id: id
+              };
+            }
+            return filter;
+          } else {
+            return {[config.base + '._id']: id};
+          }
+        }
+      });
+      this.parseTreeWithModels(method);
+    }
+    return ast;
+  }
+  fetchAll() {
+    return this.fetchItemOrItems(this.rootTree)
+    .then((results) => {
+      return this.uploadToViewsDb(results);
+    });
+  }
+  fetch(where) {
+    console.log(this.name, where);
+    let coll = this.dataSources.view.collection(this.name);
+    return coll.find(where).toArray().then((items) => {
+      return items.map((item) => ObjectID(item._id));
+    }).then((ids) => {
+      if (ids.length > 0) {
+        return this.fetchItemOrItems(this.rootTree, ids);
+      } else {
+        return [];
+      }
+    }).then((results) => {
+      if (results.length > 0) {
+        return this.updateToViewsDb(results);
+      }
+    })
+  }
+  fetchItemOrItems(config, id) {
+    let coll = this.dataSources.rest.collection(config.type);
+    let cursor = coll.find();
+    let filter = {};
+    if (id) {
+      if (Array.isArray(id)) {
+        filter = {
+          _id: {$in: id}
+        };
+      } else {
+        filter = {
+          _id: ObjectID(id)
+        };
+      }
+    }
+
+    const queryArgs = Object.assign({}, config.args);
+    if (queryArgs) {
+      delete queryArgs.$relation;
+      if (typeof queryArgs.$limit === 'number') {
+        cursor = cursor.limit(queryArgs.$limit);
+        delete queryArgs.$limit;
+      }
+      if (typeof queryArgs.$orderBy === 'string') {
+        cursor = cursor.sort({
+          [queryArgs.$orderBy]: 1
+        });
+        delete queryArgs.$orderBy;
+      }
+      if (typeof queryArgs.$lastWeekBy === 'string') {
+        queryArgs[queryArgs.$lastWeekBy] = {
+          $gt: moment().startOf('isoWeek').toDate(),
+          $lt: moment().endOf('isoWeek').toDate()
+        };
+        delete queryArgs.$lastWeekBy;
+      }
+      if (Object.keys(queryArgs).length > 0) {
+        filter = Object.assign(filter, queryArgs);
+      }
+    }
+    return cursor.filter(filter).toArray().then((items) => {
+      let filteredResults = items.map(item => {
+        return config.fields.concat('_id').reduce((newItem, field) => {
+          newItem[field] = item[field];
+          return newItem;
+        }, {});
+      }).map(item => {
+        let tasks = Object.keys(config.methods).map((methodName) => {
+          let method = config.methods[methodName];
+          let foreignKey;
+          try {
+            foreignKey = method.relation.foreignKey;
+          } catch (err) {
+            console.error(method, err);
+          }
+          let $promise;
+          switch (method.relation.type) {
+            case 'belongsTo':
+              $promise = this.fetchItemOrItems(method, item[foreignKey])
+              .then((data) => {
+                return {
+                  data: data[0],
+                  name: method.name
+                };
+              });
+              break;
+            case 'hasMany':
+              $promise = this.fetchItemOrItems(method, item[foreignKey])
+              .then((data) => {
+                return {
+                  data: data,
+                  name: method.name
+                }
+              });
+              break;
+            default:
+              $promise = null;
+          }
+          return $promise;
+        }).filter((item) => {
+          return item !== null;
+        });
+        if (tasks.length === 0) {
+          return item;
+        } else {
+          return Promise.all(tasks).then((results) => {
+            for (let result of results) {
+              item[result.name] = result.data;
+            }
+            return item;
+          });
+        }
+      });
+      return Promise.all(filteredResults);
+    });
+  }
+  uploadToViewsDb(results) {
+    let coll = this.dataSources.view.collection(this.name);
+    return coll.deleteMany({
+      _id: {$in: _.map(results, '_id')}
+    }).then(() => {
+      return coll.insertMany(results);
+    });
+  }
+  updateToViewsDb(results) {
+    let coll = this.dataSources.view.collection(this.name);
+    let upsertTasks = results.map((data) => {
+      return coll.update({_id: data.id}, data);
+    });
+    return Promise.all(upsertTasks);
+  }
+}
+
+function parseGraphs (models, dataSources) {
+  const graphPath = path.join(__dirname, './specs');
+  return glob(graphPath + '/*.graphml').map((p) => {
+    const name = path.basename(p, '.graphml');
+    const source = fs.readFileSync(p).toString('utf8');
+    return new MongoView(name, source, {
+      dataSources: dataSources,
+      models: models
+    });
+  });
+}
+
+exports.MongoView = MongoView;
+exports.parseGraphs = parseGraphs;
